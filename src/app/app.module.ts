8e580e6e3426f61855f9d192e79d2551e89d2108
dogogh <quadros1919@gmail.com>
2023-10-17 22:32:42 -0300

    opa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,10 @@ import { BannerComponent } from './Components/banner/banner.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; // Importe o módulo NgbModule aqui
 import { CollapseModule } from 'ngx-bootstrap/collapse'; // Importe o módulo CollapseModule do ngx-bootstrap
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { BebidaComponent } from './Models/bebida/bebida.component';
-import { CargoComponent } from './Models/cargo/cargo.component';
-import { CLienteComponent } from './Models/cliente/cliente.component';
-import { ComidaComponent } from './Models/comida/comida.component';
-import { EnderecoComponent } from './Models/endereco/endereco.component';
-import { FuncionarioComponent } from './Models/funcionario/funcionario.component';
-import { PedidoComponent } from './Models/pedido/pedido.component';
-import { PessoaComponent } from './Models/pessoa/pessoa.component'; // Importe o módulo BsDropdownModule do ngx-bootstrap
+
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, BannerComponent, BebidaComponent, CargoComponent, CLienteComponent, ComidaComponent, EnderecoComponent, FuncionarioComponent, PedidoComponent, PessoaComponent],
+  declarations: [AppComponent, NavComponent, BannerComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
